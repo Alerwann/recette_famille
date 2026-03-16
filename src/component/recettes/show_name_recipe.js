8e7a57046bdb76d3edd6cb03d31f5f0ac8defd31
Alerwann <alerwann411@gmail.com>
2026-03-16 12:29:06 +0100

    add photo et maj affichage liste recettes

--- src/component/recettes/show_name_recipe.js
+++ src/component/recettes/show_name_recipe.js
@@ -1,16 +1,30 @@
-import recipes from "../../data/recipes_structured.json";
-import Link from "next/link";
+import recipes from '../../data/recipes_structured.json';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function ShowNameRecipe({ typeRecipe }) {
   console.log(typeRecipe);
+
   return (
-    <div className="flex flex-col">
+    <div className="mb-5 grid grid-cols-1 md:grid-cols-3 gap-5 ">
       {recipes
         .filter((item) => item.type == typeRecipe)
         .map((item, index) => (
-          <li key={index} className="list-none">
-            <Link href="/">{item.titre}</Link>
-          </li>
+          <div
+            key={index}
+            className="flex flex-row gap-2 justify-items-center items-center text-center border-2 border-black rounded-lg hover:bg-amber-200"
+          >
+            <Link className="flex-1 p-3" href="/">
+              {item.titre}
+            </Link>
+            <Image
+              className="flex-1"
+              src={item.image ? item.image : '/fond_dessert.jpg'}
+              alt={item.titre}
+              width={50}
+              height={50}
+            />
+          </div>
         ))}
     </div>
   );
